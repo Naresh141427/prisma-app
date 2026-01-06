97f78ca603008e8d1fe51b8d5a2878dcb88a3fb5
Naresh141427 <a.nareshindian@example.com>
2026-01-06 12:10:30 +0530

    added cors for front-end use

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import express, { NextFunction, Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import cors from "cors";
+
 import logger from "./src/utils/logger";
 import requestLogger from "./src/utils/requestLogger";
 import authRoutes from "./src/routes/authRoutes";
@@ -11,9 +13,18 @@ import { env } from "./src/config/env";
 dotenv.config();
 
 const app = express();
+
 const PORT = env.PORT || 3000;
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(cookieParser());
 app.use(requestLogger);
 
